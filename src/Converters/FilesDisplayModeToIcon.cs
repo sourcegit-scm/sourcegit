c78710c7e3ab3fb07af1d422c4f480e56fcba6b3
leo <longshuang@msn.cn>
2020-11-29 22:39:44 +0800

    none<FilesDisplayModeToIcon>: code style

---
+++ src/Converters/FilesDisplayModeToIcon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SourceGit.Converters {
+
+    public class FilesDisplayModeToIcon : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var mode = (Git.Preference.FilesDisplayMode)value;
+            switch (mode) {
+            case Git.Preference.FilesDisplayMode.Grid:
+                return App.Current.FindResource("Icon.Grid") as Geometry;
+            case Git.Preference.FilesDisplayMode.List:
+                return App.Current.FindResource("Icon.List") as Geometry;
+            default:
+                return App.Current.FindResource("Icon.Tree") as Geometry;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Converters/FilesDisplayModeToVisibility.cs
+++ src/Converters/FilesDisplayModeToVisibility.cs
@@ -2,28 +2,8 @@ using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
-using System.Windows.Media;
 
 namespace SourceGit.Converters {
-
-    public class FilesDisplayModeToIcon : IValueConverter {
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            var mode = (Git.Preference.FilesDisplayMode)value;
-            switch (mode) {
-            case Git.Preference.FilesDisplayMode.Grid:
-                return App.Current.FindResource("Icon.Grid") as Geometry;
-            case Git.Preference.FilesDisplayMode.List:
-                return App.Current.FindResource("Icon.List") as Geometry;
-            default:
-                return App.Current.FindResource("Icon.Tree") as Geometry;
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotImplementedException();
-        }
-    }
 
     public class FilesDisplayModeToList : IValueConverter {
 
