0bf1261ecbbd5f48c9ba4185d723f48c2c516363
leo <longshuang@msn.cn>
2020-12-01 10:49:19 +0800

    none<build>: change publish folder

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 cd src
 
-dotnet publish -c Release -r win-x64 -o ..\publish\selfcontained\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained=true
-dotnet publish -c Release -r win-x64 -o ..\publish\no-selfcontained\ -p:PublishSingleFile=true --self-contained=false
+dotnet publish -c Release -r win-x64 -o ..\publish\SourceGit\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained=true
+dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true --self-contained=false
 
 pause
