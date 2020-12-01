f8561bb9da228f1ab6df13e650791a1435671280
leo <longshuang@msn.cn>
2020-12-01 11:17:35 +0800

    none<build>: do NOT trim output for self-contained

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 cd src
 
-dotnet publish -c Release -r win-x64 -o ..\publish\SourceGit\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained=true
+dotnet publish -c Release -r win-x64 -o ..\publish\SourceGit\ -p:PublishSingleFile=true --self-contained=true
 dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true --self-contained=false
 
 pause
