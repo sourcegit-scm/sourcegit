c6c9f62bdb37bd1d44ab704d4266aa266bdd2bc5
leo <longshuang@msn.cn>
2020-12-14 13:02:22 +0800

    update<Build>: use SatelliteResourceLanguages to trim language cultures

--- build.ps1
+++ build.ps1
@@ -1,7 +1,6 @@
 cd src
 
 dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true --self-contained=true
-Compress-Archive -Path ..\publish\SourceGit.exe -DestinationPath ..\publish\SourceGit.zip
-del /f /s /q ..\publish\SourceGit.exe
+Compress-Archive -Update -Path ..\publish\SourceGit.exe -DestinationPath ..\publish\SourceGit.zip
 
 dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true --self-contained=false
