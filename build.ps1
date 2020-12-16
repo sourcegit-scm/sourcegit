b81cf63b6fdcdaa0c4e6d66dbb9e64be95a1a6e4
leo <longshuang@msn.cn>
2020-12-16 09:23:28 +0800

    update<Build>: using TrimMode=link for publish self-contained product

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 cd src
 
-dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true --self-contained=true
+dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=link -p:IncludeNativeLibrariesForSelfExtract=true --self-contained=true
 Compress-Archive -Update -Path ..\publish\SourceGit.exe -DestinationPath ..\publish\SourceGit.zip
 
 dotnet publish -c Release -r win-x64 -o ..\publish\ -p:PublishSingleFile=true --self-contained=false
