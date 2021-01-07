5f50703f74d169fefb0111a5aa37f1cc089b7150
leo <longshuang@msn.cn>
2021-01-07 09:40:51 +0800

    style<Launcher>: style of separator between tabs in titlebar

--- src/Helpers/ChromeTabShape.cs
+++ src/Helpers/ChromeTabShape.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -21,14 +21,14 @@ namespace SourceGit.Helpers {
             var cornerSize = new Size(4, 4);
             var cornerAngle = Math.PI / 2; 
             using (var ctx = geo.Open()) {
-                ctx.BeginFigure(new Point(-5.1, ActualHeight), true, true);
-                ctx.ArcTo(new Point(-1.1, ActualHeight - 4), cornerSize, cornerAngle, false, SweepDirection.Counterclockwise, false, true);
-                ctx.LineTo(new Point(-1.1, 4), false, true);
-                ctx.ArcTo(new Point(2.9, 0), cornerSize, cornerAngle, false, SweepDirection.Clockwise, false, true);
-                ctx.LineTo(new Point(ActualWidth - 2.9, 0), false, true);
-                ctx.ArcTo(new Point(ActualWidth + 1.1, 4), cornerSize, cornerAngle, false, SweepDirection.Clockwise, false, true);
-                ctx.LineTo(new Point(ActualWidth + 1.1, ActualHeight - 4), false, true);
-                ctx.ArcTo(new Point(ActualWidth + 5.1, ActualHeight), cornerSize, cornerAngle, false, SweepDirection.Counterclockwise, false, true);
+                ctx.BeginFigure(new Point(-5, ActualHeight), true, true);
+                ctx.ArcTo(new Point(-1, ActualHeight - 4), cornerSize, cornerAngle, false, SweepDirection.Counterclockwise, false, true);
+                ctx.LineTo(new Point(-1, 4), false, true);
+                ctx.ArcTo(new Point(3, 0), cornerSize, cornerAngle, false, SweepDirection.Clockwise, false, true);
+                ctx.LineTo(new Point(ActualWidth - 4, 0), false, true);
+                ctx.ArcTo(new Point(ActualWidth, 4), cornerSize, cornerAngle, false, SweepDirection.Clockwise, false, true);
+                ctx.LineTo(new Point(ActualWidth, ActualHeight - 4), false, true);
+                ctx.ArcTo(new Point(ActualWidth + 4, ActualHeight), cornerSize, cornerAngle, false, SweepDirection.Counterclockwise, false, true);
             }
 
             geo.Freeze();
