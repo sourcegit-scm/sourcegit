3af784e45e59ff43a699acccf3a91fc49f9501f9
leo <longshuang@msn.cn>
2021-03-31 20:48:13 +0800

    update<README>: add requirement description for .NET 5.0

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 开源的Git客户端，仅用于Windows。
 
+## 注意
+
+**3.x 依赖 .NET 5，请先安装之**
+
 ## 下载
 
 [发行版](https://gitee.com/sourcegit/SourceGit/releases/)
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained false
+dotnet publish --nologo -c Release -r win-x64 -p:PublishSingleFile=true --no-self-contained -o publish
 cd ..
