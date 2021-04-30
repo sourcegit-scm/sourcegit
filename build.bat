c25ea618d009ebbc1b977da93e82f1e4c80877d2
leo <longshuang@msn.cn>
2021-04-30 12:02:21 +0800

    update<build>: update build script

--- build.bat
+++ build.bat
@@ -1,16 +1,19 @@
+@echo off
+
 rmdir /s /q publish
 
 cd src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit.csproj --nologo -c Release -r win-x64 -p:PublishSingleFile=true --no-self-contained -o ../publish
+dotnet publish SourceGit_48.csproj --nologo -c Release -r win-x64 -o ..\publish\net48
+ilrepack /ndebug /out:..\publish\SourceGit.exe ..\publish\net48\SourceGit.exe ..\publish\net48\Newtonsoft.Json.dll
+cd ..\publish
+ren SourceGit.exe SourceGit_48.exe
+rmdir /s /q net48
 
+cd ..\src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit_48.csproj --nologo -c Release -r win-x64 -o ../publish/net48
-
-cd ../publish
-ilrepack /ndebug /out:SourceGit_48.exe net48/SourceGit.exe net48/Newtonsoft.Json.dll
-rmdir /s /q net48
+dotnet publish SourceGit.csproj --nologo -c Release -r win-x64 -p:PublishSingleFile=true --no-self-contained -o ..\publish
 
 cd ../
