7551031b937a6e8e04d18a92c3f61cabc9e354b0
leo <longshuang@msn.cn>
2021-05-13 11:07:28 +0800

    update<Build>: change target runtime from win-x64 to win-x86

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rmdir /s /q publish
 cd src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit_48.csproj --nologo -c Release -r win-x64 -o ..\publish\net48
+dotnet publish SourceGit_48.csproj --nologo -c Release -r win-x86 -o ..\publish\net48
 ilrepack /ndebug /out:..\publish\SourceGit.exe ..\publish\net48\SourceGit.exe ..\publish\net48\Newtonsoft.Json.dll
 cd ..\publish
 ren SourceGit.exe SourceGit_48.exe
@@ -14,6 +14,6 @@ rmdir /s /q net48
 cd ..\src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit.csproj --nologo -c Release -r win-x64 -p:PublishSingleFile=true --no-self-contained -o ..\publish
+dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -p:PublishSingleFile=true --no-self-contained -o ..\publish
 
 cd ../
