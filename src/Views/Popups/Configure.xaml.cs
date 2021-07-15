add606291751a7cceffa647a69058c12eed926bf
leo <longshuang@msn.cn>
2021-07-15 09:23:37 +0800

    feature<Configure>: supports configuration for http.proxy

--- src/Views/Popups/Configure.xaml.cs
+++ src/Views/Popups/Configure.xaml.cs
@@ -9,6 +9,7 @@ namespace SourceGit.Views.Popups {
 
         public string UserName { get; set; }
         public string UserEmail { get; set; }
+        public string Proxy { get; set; }
 
         public Configure(string repo) {
             this.repo = repo;
@@ -16,6 +17,7 @@ namespace SourceGit.Views.Popups {
             var cmd = new Commands.Config(repo);
             UserName = cmd.Get("user.name");
             UserEmail = cmd.Get("user.email");
+            Proxy = cmd.Get("http.proxy");
 
             InitializeComponent();
         }
@@ -32,6 +34,8 @@ namespace SourceGit.Views.Popups {
                 if (oldUser != UserName) cmd.Set("user.name", UserName);
                 var oldEmail = cmd.Get("user.email");
                 if (oldEmail != UserEmail) cmd.Set("user.email", UserEmail);
+                var oldProxy = cmd.Get("http.proxy");
+                if (oldProxy != Proxy) cmd.Set("http.proxy", Proxy);
                 return true;
             });
         }
