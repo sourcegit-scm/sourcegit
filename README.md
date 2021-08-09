43ce225af191403ee1566bd4ea41ae2bc09ec1b4
leo <longshuang@msn.cn>
2021-08-09 14:21:47 +0800

    optimize<project>: thanks to Jai for providing solution - using one project to manage multi-targetframeworks

--- README.md
+++ README.md
@@ -43,11 +43,12 @@
 
 ## Thanks
 
+* [Jai](https://gitee.com/abel) 简化工程，由一个project管理多目标平台
 * [Jai](https://gitee.com/abel) .NET Framework 4.8 与 .NET 5 平台使用System.Text.Json统一代码
 * [Jai](https://gitee.com/abel) 修复SUBMODULE路径不正确的BUG
 * [Jai](https://gitee.com/abel) 修复刷新分支时，分支树节点状态未正常恢复的BUG
+* [Jai](https://gitee.com/abel) 启动恢复上次浏览页面功能
+* [Jai](https://gitee.com/abel) 修复不同remote分支结构生成错误的BUG
 * [PUMA](https://gitee.com/whgfu) 配置默认User
 * [Rwing](https://gitee.com/rwing) GitFlow: add an option to keep branch after finish
 * [XiaoLinger](https://gitee.com/LingerNN) 纠正弹出框文本配置方式
-* [Jai](https://gitee.com/abel) 启动恢复上次浏览页面功能
-* [Jai](https://gitee.com/abel) 修复不同remote分支结构生成错误的BUG
--- build.bat
+++ build.bat
@@ -5,15 +5,17 @@ rmdir /s /q publish
 cd src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit_48.csproj --nologo -c Release -r win-x86 -o ..\publish\net48
+dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -f net48 -o ..\publish\net48
 ilrepack /ndebug /wildcards /out:..\publish\SourceGit.exe ..\publish\net48\SourceGit.exe ..\publish\net48\*.dll
 cd ..\publish
-ren SourceGit.exe SourceGit_48.exe
+ren SourceGit.exe SourceGit.net48.exe
 rmdir /s /q net48
 
 cd ..\src
 rmdir /s /q bin
 rmdir /s /q obj
-dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -p:PublishSingleFile=true --no-self-contained -o ..\publish
+dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -f net5.0-windows -p:PublishSingleFile=true --no-self-contained -o ..\publish
+cd ..\publish
+ren SourceGit.exe SourceGit.net50.exe
 
 cd ../
