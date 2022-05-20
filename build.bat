1a1d0d900495a27f0d64aa1f2a1a1550d2e70bf1
leo <longshuang@msn.cn>
2022-05-20 15:49:58 +0800

    refactor<build>: no longer provides .NET48 and .NET6 x86 targets.

--- build.bat
+++ build.bat
@@ -3,26 +3,7 @@
 rmdir /s /q publish
 
 cd src
-rmdir /s /q bin
-rmdir /s /q obj
-dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -f net48 -o ..\publish\net48
-ilrepack /ndebug /wildcards /out:..\publish\SourceGit.exe ..\publish\net48\SourceGit.exe ..\publish\net48\*.dll
-cd ..\publish
-ren SourceGit.exe SourceGit.net4.8.exe
-rmdir /s /q net48
-
-cd ..\src
-rmdir /s /q bin
-rmdir /s /q obj
-dotnet publish SourceGit.csproj --nologo -c Release -r win-x86 -f net6.0-windows -p:PublishSingleFile=true --no-self-contained -o ..\publish
-cd ..\publish
-ren SourceGit.exe SourceGit.net6.0.x86.exe
-
-cd ..\src
-rmdir /s /q bin
-rmdir /s /q obj
 dotnet publish SourceGit.csproj --nologo -c Release -r win-x64 -f net6.0-windows -p:PublishSingleFile=true --no-self-contained -o ..\publish
-cd ..\publish
-ren SourceGit.exe SourceGit.net6.0.x64.exe
+dotnet publish SourceGit.csproj --nologo -c Release -r win-x64 -f net6.0-windows --self-contained -o ..\publish\SourceGit
 
-cd ../
+cd ..
