54b9c420c170c06b40dcbf8ac2f8556488608312
leo <longshuang@msn.cn>
2024-04-01 15:27:55 +0800

    build: use Powershell script instead of batch file to generate packages on Windows and remove dependency of 7z (#43)

---
+++ build/build.windows.ps1
@@ -0,0 +1,15 @@
+if (Test-Path SourceGit) {
+    Remove-Item SourceGit -Recurse -Force
+}
+
+if (Test-Path SourceGit.win-x64.zip) {
+    Remove-Item SourceGit.win-x64.zip -Force
+}
+
+dotnet publish ..\src\SourceGit\SourceGit.csproj -c Release -r win-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+
+Remove-Item SourceGit\*.pdb -Force
+Remove-Item SourceGit\zh -Recurse -Force
+Remove-Item SourceGit\en -Recurse -Force
+
+Compress-Archive -Path SourceGit -DestinationPath SourceGit.win-x64.zip
