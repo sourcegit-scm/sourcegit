96d4150d26fc2012cac2306eda0bffc92dd37185
leo <longshuang@msn.cn>
2024-04-02 20:00:33 +0800

    project: reorganize the structure of the project.
    * remove dotnet-tool.json because the project does not rely on any dotnet tools.
    * remove Directory.Build.props because the solution has only one project.
    * move src/SourceGit to src. It's not needed to put all sources into a subfolder of src since there's only one project.

--- build/build.linux.sh
+++ build/build.linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf SourceGit
-dotnet publish ../src/SourceGit/SourceGit.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+dotnet publish ../src/SourceGit.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 cp resources/SourceGit.desktop.template SourceGit/SourceGit.desktop.template
 cp resources/App.icns SourceGit/SourceGit.icns
 tar -zcvf SourceGit.linux-x64.tar.gz --exclude="*/en" --exclude="*/zh" --exclude="*/*.dbg" SourceGit
--- build/build.windows.ps1
+++ build/build.windows.ps1
@@ -6,7 +6,7 @@ if (Test-Path SourceGit.win-x64.zip) {
     Remove-Item SourceGit.win-x64.zip -Force
 }
 
-dotnet publish ..\src\SourceGit\SourceGit.csproj -c Release -r win-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+dotnet publish ..\src\SourceGit.csproj -c Release -r win-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
 
 Remove-Item SourceGit\*.pdb -Force
 Remove-Item SourceGit\zh -Recurse -Force
