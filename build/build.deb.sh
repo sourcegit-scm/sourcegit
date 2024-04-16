ebf139de68a583058bb2f5754f1f4c4bccb3f0db
leo <longshuang@msn.cn>
2024-04-16 12:37:15 +0800

    feature: add a deb build script for Linux

---
+++ build/build.deb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf SourceGit resources/deb/opt
+dotnet publish ../src/SourceGit.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
+
+mkdir -p resources/deb/opt/sourcegit
+mv SourceGit/SourceGit resources/deb/opt/sourcegit/sourcegit
+mv SourceGit/*.so resources/deb/opt/sourcegit/
+
+chmod -R 755 resources/deb
+
+rm -rf SourceGit
+
+sudo dpkg-deb --build resources/deb
--- build/build.linux.sh
+++ build/build.linux.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-rm -rf SourceGit
+rm -rf SourceGit SourceGit.linux-x64.tar.gz
 dotnet publish ../src/SourceGit.csproj -c Release -r linux-x64 -o SourceGit -p:PublishAot=true -p:PublishTrimmed=true -p:TrimMode=link --self-contained
-cp resources/SourceGit.desktop.template SourceGit/SourceGit.desktop.template
-cp resources/App.icns SourceGit/SourceGit.icns
 tar -zcvf SourceGit.linux-x64.tar.gz --exclude="*/*.dbg" SourceGit
 rm -rf SourceGit
