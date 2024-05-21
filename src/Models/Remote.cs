717e6b4fafbfe7dc803624458b0fd33f4eda704f
leo <longshuang@msn.cn>
2024-05-21 14:35:14 +0800

    enhance: a more adaptable regular expression for remote URLs (#136)

--- src/Models/Remote.cs
+++ src/Models/Remote.cs
@@ -6,9 +6,9 @@ namespace SourceGit.Models
     {
         [GeneratedRegex(@"^http[s]?://([\w\-]+@)?[\w\.\-]+(\:[0-9]+)?/[\w\-/]+/[\w\-\.]+\.git$")]
         private static partial Regex REG_HTTPS();
-        [GeneratedRegex(@"^[\w\-]+@[\w\.\-]+(\:[0-9]+)?:[\w\-]+/[\w\-\.]+\.git$")]
+        [GeneratedRegex(@"^[\w\-]+@[\w\.\-]+(\:[0-9]+)?:[\w\-/]+/[\w\-\.]+\.git$")]
         private static partial Regex REG_SSH1();
-        [GeneratedRegex(@"^ssh://([\w\-]+@)?[\w\.\-]+(\:[0-9]+)?/[\w\-]+/[\w\-\.]+\.git$")]
+        [GeneratedRegex(@"^ssh://([\w\-]+@)?[\w\.\-]+(\:[0-9]+)?/[\w\-/]+/[\w\-\.]+\.git$")]
         private static partial Regex REG_SSH2();
 
         private static readonly Regex[] URL_FORMATS = [
