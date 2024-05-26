dc63e39c69e1a3aa9628ec08de3a58a6c6ff61d9
leo <longshuang@msn.cn>
2024-05-26 12:17:15 +0800

    fix: warning about EnumConverters.Equals

--- src/Converters/EnumConverters.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls.Converters;
-
-namespace SourceGit.Converters
-{
-    public static class EnumConverters
-    {
-        public static readonly EnumToBoolConverter Equals = new EnumToBoolConverter();
-    }
-}
