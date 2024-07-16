6d888f8007a99918e5de3c769b54f2c110ef8760
leo <longshuang@msn.cn>
2024-07-16 17:03:36 +0800

    code_style: remove unused namespace using

--- src/Views/ChangeCollectionView.axaml.cs
+++ src/Views/ChangeCollectionView.axaml.cs
@@ -6,7 +6,6 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Threading;
 using Avalonia.VisualTree;
 
 namespace SourceGit.Views
