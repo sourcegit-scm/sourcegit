edf09234c8406e9c3b2ada41f3c618e4755577bb
warappa <spoiltnr1@gmx.at>
2024-07-31 23:28:58 +0200

    #305: Add German translation

--- src/Models/Locales.cs
+++ src/Models/Locales.cs
@@ -11,6 +11,7 @@ namespace SourceGit.Models
             new Locale("English", "en_US"),
             new Locale("简体中文", "zh_CN"),
             new Locale("繁體中文", "zh_TW"),
+            new Locale("Deutsch", "de_DE")
         };
 
         public Locale(string name, string key)
