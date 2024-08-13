a145d6e4c379ce0cce81d1db96ef2ffda2987e7b
leo <longshuang@msn.cn>
2024-08-13 15:58:34 +0800

    revert: remove issue link support in commit list
    * improve render performance
    * the links in commit list are very easy to click by mistake

--- src/ViewModels/Histories.cs
+++ src/ViewModels/Histories.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;
 using Avalonia.VisualTree;
@@ -53,11 +52,6 @@ namespace SourceGit.ViewModels
         {
             get => _detailContext;
             set => SetProperty(ref _detailContext, value);
-        }
-
-        public AvaloniaList<Models.IssueTrackerRule> IssueTrackerRules
-        {
-            get => _repo.Settings.IssueTrackerRules;
         }
 
         public Histories(Repository repo)
