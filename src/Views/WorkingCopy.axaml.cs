c22ba757800f00fd515993e0fcec7329c39a28ef
leo <longshuang@msn.cn>
2024-08-29 10:10:28 +0800

    enhance: supports using `Delete` or `Backspace` to discard selected changes (#423)

--- src/Views/WorkingCopy.axaml.cs
+++ src/Views/WorkingCopy.axaml.cs
@@ -62,10 +62,21 @@ namespace SourceGit.Views
 
         private void OnUnstagedKeyDown(object _, KeyEventArgs e)
         {
-            if (DataContext is ViewModels.WorkingCopy vm && e.Key is Key.Space or Key.Enter)
+            if (DataContext is ViewModels.WorkingCopy vm)
             {
-                vm.StageSelected();
-                e.Handled = true;
+                if (e.Key is Key.Space or Key.Enter)
+                {
+                    vm.StageSelected();
+                    e.Handled = true;
+                    return;
+                }
+
+                if (e.Key is Key.Delete or Key.Back && vm.SelectedUnstaged is { Count: > 0 } selected)
+                {
+                    vm.Discard(selected);
+                    e.Handled = true;
+                    return;
+                }
             }
         }
 
