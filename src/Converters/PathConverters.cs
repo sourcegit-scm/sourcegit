81b72f7c1ccee0a4e33062f731e865d8aea6cceb
leo <longshuang@msn.cn>
2024-09-22 11:00:30 +0800

    enhance: use `~` to represent the home dir of current user

--- src/Converters/PathConverters.cs
+++ src/Converters/PathConverters.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 using Avalonia.Data.Converters;
 
@@ -7,9 +8,23 @@ namespace SourceGit.Converters
     public static class PathConverters
     {
         public static readonly FuncValueConverter<string, string> PureFileName =
-            new FuncValueConverter<string, string>(fullpath => Path.GetFileName(fullpath) ?? "");
+            new(v => Path.GetFileName(v) ?? "");
 
         public static readonly FuncValueConverter<string, string> PureDirectoryName =
-            new FuncValueConverter<string, string>(fullpath => Path.GetDirectoryName(fullpath) ?? "");
+            new(v => Path.GetDirectoryName(v) ?? "");
+
+        public static readonly FuncValueConverter<string, string> RelativeToHome =
+            new(v =>
+            {
+                if (OperatingSystem.IsWindows())
+                    return v;
+                
+                var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                var prefixLen = home.EndsWith('/') ? home.Length - 1 : home.Length;
+                if (v.StartsWith(home, StringComparison.Ordinal))
+                    return "~" + v.Substring(prefixLen);
+
+                return v;
+            });
     }
 }
