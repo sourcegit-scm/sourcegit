a8685f2047bb226761453d0db57cb4c0cc82372f
leo <longshuang@msn.cn>
2024-09-25 10:35:07 +0800

    refactor: rewrite the way to support HomeBrew pathes on macOS (#507)

--- src/Commands/Command.cs
+++ src/Commands/Command.cs
@@ -68,6 +68,10 @@ namespace SourceGit.Commands
             if (OperatingSystem.IsLinux())
                 start.Environment.Add("LANG", "en_US.UTF-8");
 
+            // Fix sometimes `LSEnvironment` not working on macOS
+            if (OperatingSystem.IsMacOS())
+                start.Environment.Add("PATH", "/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin");
+
             // Force using this app as git editor.
             switch (Editor)
             {
