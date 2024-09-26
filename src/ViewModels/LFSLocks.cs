5d2a4421442d078a42b16170d645e77776603c1a
leo <longshuang@msn.cn>
2024-09-26 15:36:20 +0800

    code_review: PR #515
    * remove Linq expressions due to AOT limitations. See https://learn.microsoft.com/zh-cn/dotnet/core/deploying/native-aot/?tabs=windows%2Cnet8#limitations-of-native-aot-deployment
    * rename `FilteredLocks` to `VisibleLocks`
    * use `Commands.Config.Get` instead of `Commands.Config.ListAll`
    * disable checkbox if user name is not valid

--- src/ViewModels/LFSLocks.cs
+++ src/ViewModels/LFSLocks.cs
@@ -1,7 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using Avalonia.Collections;
 using Avalonia.Threading;
 
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -10,16 +9,16 @@ namespace SourceGit.ViewModels
 {
     public class LFSLocks : ObservableObject
     {
+        public bool HasValidUserName
+        {
+            get;
+            private set;
+        } = false;
+
         public bool IsLoading
         {
             get => _isLoading;
             private set => SetProperty(ref _isLoading, value);
-        }
-
-        public bool IsEmpty
-        {
-            get => _isEmpty;
-            private set => SetProperty(ref _isEmpty, value);
         }
         
         public bool ShowOnlyMyLocks
@@ -27,52 +26,32 @@ namespace SourceGit.ViewModels
             get => _showOnlyMyLocks;
             set
             {
-                if (!SetProperty(ref _showOnlyMyLocks, value))
-                    return;
-
-                OnPropertyChanged(nameof(FilteredLocks));
-                IsEmpty = !FilteredLocks.Any();
+                if (SetProperty(ref _showOnlyMyLocks, value))
+                    UpdateVisibleLocks();
             }
         }
 
-        private AvaloniaList<Models.LFSLock> Locks
+        public List<Models.LFSLock> VisibleLocks
         {
-            get;
-        }
-
-        public AvaloniaList<Models.LFSLock> FilteredLocks
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_userName))
-                {
-                    App.RaiseException(_repo, "Username is empty");
-                    return Locks;
-                }
-
-                return _showOnlyMyLocks ?
-                    new AvaloniaList<Models.LFSLock>(Locks.Where(@lock => @lock.User == _userName)) :
-                    Locks;
-            }
+            get => _visibleLocks;
+            private set => SetProperty(ref _visibleLocks, value);
         }
 
         public LFSLocks(string repo, string remote)
         {
             _repo = repo;
             _remote = remote;
-            Locks = new AvaloniaList<Models.LFSLock>();
-            new Commands.Config(repo).ListAll().TryGetValue("user.name", out _userName);
+            _userName = new Commands.Config(repo).Get("user.name");
+
+            HasValidUserName = !string.IsNullOrEmpty(_userName);
 
             Task.Run(() =>
             {
-                var collect = new Commands.LFS(_repo).Locks(_remote);
+                _cachedLocks = new Commands.LFS(_repo).Locks(_remote);
                 Dispatcher.UIThread.Invoke(() =>
                 {
-                    if (collect.Count > 0)
-                        Locks.AddRange(collect);
-
+                    UpdateVisibleLocks();
                     IsLoading = false;
-                    IsEmpty = collect.Count == 0;
                 });
             });
         }
@@ -89,18 +68,40 @@ namespace SourceGit.ViewModels
                 Dispatcher.UIThread.Invoke(() =>
                 {
                     if (succ)
-                        Locks.Remove(lfsLock);
+                    {
+                        _cachedLocks.Remove(lfsLock);
+                        UpdateVisibleLocks();
+                    }
 
                     IsLoading = false;
-                    IsEmpty = Locks.Count == 0;
                 });
             });
+        }
+
+        private void UpdateVisibleLocks()
+        {
+            if (!_showOnlyMyLocks)
+            {
+                VisibleLocks = _cachedLocks;
+            }
+            else
+            {
+                var visible = new List<Models.LFSLock>();
+                foreach (var lfsLock in _cachedLocks)
+                {
+                    if (lfsLock.User == _userName)
+                        visible.Add(lfsLock);
+                }
+
+                VisibleLocks = visible;
+            }
         }
 
         private string _repo;
         private string _remote;
         private bool _isLoading = true;
-        private bool _isEmpty = false;
+        private List<Models.LFSLock> _cachedLocks = [];
+        private List<Models.LFSLock> _visibleLocks = [];
         private bool _showOnlyMyLocks = false;
         private string _userName;
     }
