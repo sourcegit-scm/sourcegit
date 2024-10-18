9153bbe07f90935b9661aed75df5f44421d65215
leo <longshuang@msn.cn>
2024-10-18 16:20:45 +0800

    feature: add conventional commit message generator (#574)

---
+++ src/Models/ConventionalCommitType.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace SourceGit.Models
+{
+    public class ConventionalCommitType
+    {
+        public string Type { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public static readonly List<ConventionalCommitType> Supported = new List<ConventionalCommitType>()
+        {
+            new ConventionalCommitType("feat", "Adding a new feature"),
+            new ConventionalCommitType("fix", "Fixing a bug"),
+            new ConventionalCommitType("docs", "Updating documentation"),
+            new ConventionalCommitType("style", "Elements or code styles without changing the code logic"),
+            new ConventionalCommitType("test", "Adding or updating tests"),
+            new ConventionalCommitType("chore", "Making changes to the build process or auxiliary tools and libraries"),
+            new ConventionalCommitType("revert", "Undoing a previous commit"),
+            new ConventionalCommitType("refactor", "Restructuring code without changing its external behavior")
+        };
+
+        public ConventionalCommitType(string type, string description)
+        {
+            Type = type;
+            Description = description;
+        }
+    }
+}
---
+++ src/ViewModels/ConventionalCommitMessageBuilder.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SourceGit.ViewModels
+{
+    public class ConventionalCommitMessageBuilder : ObservableValidator
+    {
+        [Required(ErrorMessage = "Type of changes can not be null")]
+        public Models.ConventionalCommitType Type
+        {
+            get => _type;
+            set => SetProperty(ref _type, value, true);
+        }
+
+        public string Scope
+        {
+            get => _scope;
+            set => SetProperty(ref _scope, value);
+        }
+
+        [Required(ErrorMessage = "Short description can not be empty")]
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value, true);
+        }
+
+        public string Detail
+        {
+            get => _detail;
+            set => SetProperty(ref _detail, value);
+        }
+
+        public string BreakingChanges
+        {
+            get => _breakingChanges;
+            set => SetProperty(ref _breakingChanges, value);
+        }
+
+        public string ClosedIssue
+        {
+            get => _closedIssue;
+            set => SetProperty(ref _closedIssue, value);
+        }
+
+        public ConventionalCommitMessageBuilder(WorkingCopy wc)
+        {
+            _wc = wc;
+        }
+
+        [UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2026:RequiresUnreferencedCode")]
+        public bool Apply()
+        {
+            if (HasErrors)
+                return false;
+
+            ValidateAllProperties();
+            if (HasErrors)
+                return false;
+
+            var builder = new StringBuilder();
+            builder.Append(_type.Type);
+
+            if (!string.IsNullOrEmpty(_scope))
+            {
+                builder.Append("(");
+                builder.Append(_scope);
+                builder.Append("): ");
+            }
+            else
+            {
+                builder.Append(": ");
+            }
+
+            builder.Append(_description);
+            builder.Append("\n\n");
+
+            if (!string.IsNullOrEmpty(_detail))
+            {
+                builder.Append(_detail);
+                builder.Append("\n\n");
+            }
+
+            if (!string.IsNullOrEmpty(_breakingChanges))
+            {
+                builder.Append("BREAKING CHANGE: ");
+                builder.Append(_breakingChanges);
+                builder.Append("\n\n");
+            }
+
+            if (!string.IsNullOrEmpty(_closedIssue))
+            {
+                builder.Append("Closed ");
+                builder.Append(_closedIssue);
+            }
+
+            _wc.CommitMessage = builder.ToString();
+            return true;
+        }
+
+        private WorkingCopy _wc = null;
+        private Models.ConventionalCommitType _type = Models.ConventionalCommitType.Supported[0];
+        private string _scope = string.Empty;
+        private string _description = string.Empty;
+        private string _detail = string.Empty;
+        private string _breakingChanges = string.Empty;
+        private string _closedIssue = string.Empty;
+    }
+}
