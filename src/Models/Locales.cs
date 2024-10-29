36adc9b4f5e7316149e7fdce3fb96b753303baa4
adelacruz <albertodelacruz96@gmail.com>
2024-10-29 22:26:59 +0100

    feat: initial spanish (Spain) location
    - WIP

--- src/Models/Locales.cs
+++ src/Models/Locales.cs
@@ -13,6 +13,7 @@ namespace SourceGit.Models
             new Locale("Français", "fr_FR"),
             new Locale("Português (Brasil)", "pt_BR"),
             new Locale("Русский", "ru_RU"),
+            new Locale("Español (España)", "es_ES"),
             new Locale("简体中文", "zh_CN"),
             new Locale("繁體中文", "zh_TW"),
         };
