ea1d966d27580e9a970ad919b6bde3652c9f2da1
Enner Pérez <ennerperez@gmail.com>
2024-11-18 20:14:53 -0500

    feat: Reset Mode Hotkey (#714)

--- src/Views/Reset.axaml.cs
+++ src/Views/Reset.axaml.cs
@@ -1,4 +1,8 @@
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using SourceGit.Models;
 
 namespace SourceGit.Views
 {
@@ -7,6 +11,29 @@ namespace SourceGit.Views
         public Reset()
         {
             InitializeComponent();
+        }
+
+        private void InputElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var key = e.Key.ToString().ToLower();
+            foreach (var item in ResetMode.ItemsSource)
+            {
+                if (item.GetType() == typeof(ResetMode))
+                {
+                    var resetMode = (ResetMode)item;
+                    if (resetMode.Key.ToString().ToLower() == key)
+                    {
+                        ResetMode.SelectedValue = resetMode;
+                        return;
+                    }
+                        
+                }
+            }
+        }
+
+        private void Control_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ResetMode.Focus();
         }
     }
 }
