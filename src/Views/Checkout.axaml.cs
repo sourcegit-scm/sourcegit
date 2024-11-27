1872148d985515f3bd50938aacffec47cd2688ae
leo <longshuang@msn.cn>
2024-11-27 18:31:50 +0800

    fix: the way to deal with local changes did not update after radio toggle changed (#748)

--- src/Views/Checkout.axaml.cs
+++ src/Views/Checkout.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace SourceGit.Views
 {
@@ -7,6 +8,52 @@ namespace SourceGit.Views
         public Checkout()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoaded(RoutedEventArgs e)
+        {
+            base.OnLoaded(e);
+
+            var vm = DataContext as ViewModels.Checkout;
+            if (vm == null)
+                return;
+
+            switch (vm.PreAction)
+            {
+                case Models.DealWithLocalChanges.DoNothing:
+                    RadioDoNothing.IsChecked = true;
+                    break;
+                case Models.DealWithLocalChanges.StashAndReaply:
+                    RadioStashAndReply.IsChecked = true;
+                    break;
+                default:
+                    RadioDiscard.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void OnLocalChangeActionIsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as ViewModels.Checkout;
+            if (vm == null)
+                return;
+
+            if (RadioDoNothing.IsChecked == true)
+            {
+                if (vm.PreAction != Models.DealWithLocalChanges.DoNothing)
+                    vm.PreAction = Models.DealWithLocalChanges.DoNothing;
+                return;
+            }
+
+            if (RadioStashAndReply.IsChecked == true)
+            {
+                if (vm.PreAction != Models.DealWithLocalChanges.StashAndReaply)
+                    vm.PreAction = Models.DealWithLocalChanges.StashAndReaply;
+                return;
+            }
+
+            if (vm.PreAction != Models.DealWithLocalChanges.Discard)
+                vm.PreAction = Models.DealWithLocalChanges.Discard;
         }
     }
 }
