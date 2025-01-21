90c04f1db2869e53fc0c5f8434e17c3907c3d5e6
aikawayataro <aikawayataro@protonmail.com>
2025-01-21 12:01:09 +0000

    fix: `_` is not parsed in template variable name (#920)

--- src/Models/TemplateEngine.cs
+++ src/Models/TemplateEngine.cs
@@ -313,7 +313,7 @@ namespace SourceGit.Models
 
         private static bool IsNameChar(char c)
         {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_';
         }
 
         // (?) notice or log if variable is not found
