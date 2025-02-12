c91c483c2ef8fd1e3fa823f0b784e972dd05d982
Dmitrij D. Czarkoff <czarkoff@gmail.com>
2025-02-13 00:02:56 +0100

    feat: change worktree presentation
    Present the worktree name first, then relative path to the main repo.
    
    This is more aligned with Git's own UI,  and works better with UI size constrains.

--- src/Models/Worktree.cs
+++ src/Models/Worktree.cs
@@ -6,6 +6,7 @@ namespace SourceGit.Models
     {
         public string Branch { get; set; } = string.Empty;
         public string FullPath { get; set; } = string.Empty;
+        public string RelativePath { get; set; } = string.Empty;
         public string Head { get; set; } = string.Empty;
         public bool IsBare { get; set; } = false;
         public bool IsDetached { get; set; } = false;
@@ -21,15 +22,15 @@ namespace SourceGit.Models
             get
             {
                 if (IsDetached)
-                    return $"(deteched HEAD at {Head.Substring(10)})";
+                    return $"deteched HEAD at {Head.Substring(10)}";
 
                 if (Branch.StartsWith("refs/heads/", System.StringComparison.Ordinal))
-                    return $"({Branch.Substring(11)})";
+                    return Branch.Substring(11);
 
                 if (Branch.StartsWith("refs/remotes/", System.StringComparison.Ordinal))
-                    return $"({Branch.Substring(13)})";
+                    return Branch.Substring(13);
 
-                return $"({Branch})";
+                return Branch;
             }
         }
 
