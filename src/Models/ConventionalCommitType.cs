cac4b7edf60d9c4131f8445d5b02182c73d47107
leo <longshuang@msn.cn>
2025-04-16 15:39:47 +0800

    enhance: conventional commit message helper (#1200)
    - add `build`, `ci`, `pref`
    - re-design helper dialog
    
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Models/ConventionalCommitType.cs
+++ src/Models/ConventionalCommitType.cs
@@ -4,23 +4,29 @@ namespace SourceGit.Models
 {
     public class ConventionalCommitType
     {
+        public string Name { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
 
         public static readonly List<ConventionalCommitType> Supported = new List<ConventionalCommitType>()
         {
-            new ConventionalCommitType("feat", "Adding a new feature"),
-            new ConventionalCommitType("fix", "Fixing a bug"),
-            new ConventionalCommitType("docs", "Updating documentation"),
-            new ConventionalCommitType("style", "Elements or code styles without changing the code logic"),
-            new ConventionalCommitType("test", "Adding or updating tests"),
-            new ConventionalCommitType("chore", "Making changes to the build process or auxiliary tools and libraries"),
-            new ConventionalCommitType("revert", "Undoing a previous commit"),
-            new ConventionalCommitType("refactor", "Restructuring code without changing its external behavior")
+            new ConventionalCommitType("Features", "feat", "Adding a new feature"),
+            new ConventionalCommitType("Bug Fixes", "fix", "Fixing a bug"),
+            new ConventionalCommitType("Reverts", "revert", "Undoing a previous commit"),
+            new ConventionalCommitType("Code Refactoring", "refactor", "Restructuring code without changing its external behavior"),
+            new ConventionalCommitType("Performance Improvements", "pref", "Improves performance"),
+            new ConventionalCommitType("Builds", "build", "Changes that affect the build system or external dependencies"),
+            new ConventionalCommitType("Continuous Integrations", "ci", "Changes to CI configuration files and scripts"),
+            new ConventionalCommitType("Documentations", "docs", "Updating documentation"),
+            new ConventionalCommitType("Styles", "style", "Elements or code styles without changing the code logic"),
+            new ConventionalCommitType("Tests", "test", "Adding or updating tests"),
+            new ConventionalCommitType("Chores", "chore", "Other changes that don't modify src or test files"),
+
         };
 
-        public ConventionalCommitType(string type, string description)
+        public ConventionalCommitType(string name, string type, string description)
         {
+            Name = name;
             Type = type;
             Description = description;
         }
