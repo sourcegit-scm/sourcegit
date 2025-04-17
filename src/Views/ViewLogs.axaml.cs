9f493abd1aa3efc388f40904217aae7b8ac4d379
leo <longshuang@msn.cn>
2025-04-17 16:30:47 +0800

    enhance: add context menu for selected log
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Views/ViewLogs.axaml.cs
+++ src/Views/ViewLogs.axaml.cs
@@ -97,10 +97,33 @@ namespace SourceGit.Views
             InitializeComponent();
         }
 
-        private void OnRemoveLog(object sender, RoutedEventArgs e)
+        private void OnLogContextRequested(object sender, ContextRequestedEventArgs e)
         {
-            if (sender is Button { DataContext: ViewModels.CommandLog log } && DataContext is ViewModels.ViewLogs vm)
+            if (sender is not Grid { DataContext: ViewModels.CommandLog log } grid || DataContext is not ViewModels.ViewLogs vm)
+                return;
+
+            var copy = new MenuItem();
+            copy.Header = App.Text("ViewLogs.CopyLog");
+            copy.Icon = App.CreateMenuIcon("Icons.Copy");
+            copy.Click += (_, ev) =>
+            {
+                App.CopyText(log.Content);
+                ev.Handled = true;
+            };
+
+            var rm = new MenuItem();
+            rm.Header = App.Text("ViewLogs.Delete");
+            rm.Icon = App.CreateMenuIcon("Icons.Clear");
+            rm.Click += (_, ev) =>
+            {
                 vm.Logs.Remove(log);
+                ev.Handled = true;
+            };
+
+            var menu = new ContextMenu();
+            menu.Items.Add(copy);
+            menu.Items.Add(rm);
+            menu.Open(grid);
 
             e.Handled = true;
         }
