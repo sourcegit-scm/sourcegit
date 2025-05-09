15ee2dac91298485c4a45e85f07fbdf88a02ce31
qiufengshe <172344058@qq.com>
2025-05-09 09:19:33 +0800

    perf: minimize temporary strings for better performance (#1292)

--- src/Models/RepositorySettings.cs
+++ src/Models/RepositorySettings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -344,28 +344,28 @@ namespace SourceGit.Models
                     if (filter.Mode == FilterMode.Included)
                         includedRefs.Add(filter.Pattern);
                     else if (filter.Mode == FilterMode.Excluded)
-                        excludedBranches.Add($"--exclude=\"{filter.Pattern.Substring(11)}\" --decorate-refs-exclude=\"{filter.Pattern}\"");
+                        excludedBranches.Add($"--exclude=\"{filter.Pattern.AsSpan().Slice(11)}\" --decorate-refs-exclude=\"{filter.Pattern}\"");
                 }
                 else if (filter.Type == FilterType.LocalBranchFolder)
                 {
                     if (filter.Mode == FilterMode.Included)
-                        includedRefs.Add($"--branches={filter.Pattern.Substring(11)}/*");
+                        includedRefs.Add($"--branches={filter.Pattern.AsSpan().Slice(11)}/*");
                     else if (filter.Mode == FilterMode.Excluded)
-                        excludedBranches.Add($"--exclude=\"{filter.Pattern.Substring(11)}/*\" --decorate-refs-exclude=\"{filter.Pattern}/*\"");
+                        excludedBranches.Add($"--exclude=\"{filter.Pattern.AsSpan().Slice(11)}/*\" --decorate-refs-exclude=\"{filter.Pattern}/*\"");
                 }
                 else if (filter.Type == FilterType.RemoteBranch)
                 {
                     if (filter.Mode == FilterMode.Included)
                         includedRefs.Add(filter.Pattern);
                     else if (filter.Mode == FilterMode.Excluded)
-                        excludedRemotes.Add($"--exclude=\"{filter.Pattern.Substring(13)}\" --decorate-refs-exclude=\"{filter.Pattern}\"");
+                        excludedRemotes.Add($"--exclude=\"{filter.Pattern.AsSpan().Slice(13)}\" --decorate-refs-exclude=\"{filter.Pattern}\"");
                 }
                 else if (filter.Type == FilterType.RemoteBranchFolder)
                 {
                     if (filter.Mode == FilterMode.Included)
-                        includedRefs.Add($"--remotes={filter.Pattern.Substring(13)}/*");
+                        includedRefs.Add($"--remotes={filter.Pattern.AsSpan().Slice(13)}/*");
                     else if (filter.Mode == FilterMode.Excluded)
-                        excludedRemotes.Add($"--exclude=\"{filter.Pattern.Substring(13)}/*\" --decorate-refs-exclude=\"{filter.Pattern}/*\"");
+                        excludedRemotes.Add($"--exclude=\"{filter.Pattern.AsSpan().Slice(13)}/*\" --decorate-refs-exclude=\"{filter.Pattern}/*\"");
                 }
                 else if (filter.Type == FilterType.Tag)
                 {
