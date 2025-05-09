029fd6933f3c86afec18ab88b98e2c38ba55ba09
leo <longshuang@msn.cn>
2025-05-09 22:57:46 +0800

    refactor: new way to discard selected or all local changes
    This modification aims to solve the problem that the deleted submodule cannot be discarded.
    
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Commands/Discard.cs
+++ src/Commands/Discard.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
+using Avalonia.Threading;
 
 namespace SourceGit.Commands
 {
@@ -7,33 +10,71 @@ namespace SourceGit.Commands
     {
         public static void All(string repo, bool includeIgnored, Models.ICommandLog log)
         {
+            var changes = new QueryLocalChanges(repo).Result();
+            try
+            {
+                foreach (var c in changes)
+                {
+                    if (c.WorkTree == Models.ChangeState.Untracked ||
+                        c.WorkTree == Models.ChangeState.Added ||
+                        c.Index == Models.ChangeState.Added ||
+                        c.Index == Models.ChangeState.Renamed)
+                    {
+                        var fullPath = Path.Combine(repo, c.Path);
+                        if (Directory.Exists(fullPath))
+                            Directory.Delete(fullPath, true);
+                        else
+                            File.Delete(fullPath);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Dispatcher.UIThread.Invoke(() =>
+                {
+                    App.RaiseException(repo, $"Failed to discard changes. Reason: {e.Message}");
+                });
+            }
+            
             new Restore(repo) { Log = log }.Exec();
-            new Clean(repo, includeIgnored) { Log = log }.Exec();
+            if (includeIgnored)
+                new Clean(repo) { Log = log }.Exec();
         }
 
         public static void Changes(string repo, List<Models.Change> changes, Models.ICommandLog log)
         {
-            var needClean = new List<string>();
-            var needCheckout = new List<string>();
+            var restores = new List<string>();
 
-            foreach (var c in changes)
+            try
             {
-                if (c.WorkTree == Models.ChangeState.Untracked || c.WorkTree == Models.ChangeState.Added)
-                    needClean.Add(c.Path);
-                else
-                    needCheckout.Add(c.Path);
+                foreach (var c in changes)
+                {
+                    if (c.WorkTree == Models.ChangeState.Untracked || c.WorkTree == Models.ChangeState.Added)
+                    {
+                        var fullPath = Path.Combine(repo, c.Path);
+                        if (Directory.Exists(fullPath))
+                            Directory.Delete(fullPath, true);
+                        else
+                            File.Delete(fullPath);
+                    }
+                    else
+                    {
+                        restores.Add(c.Path);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Dispatcher.UIThread.Invoke(() =>
+                {
+                    App.RaiseException(repo, $"Failed to discard changes. Reason: {e.Message}");
+                });
             }
 
-            for (int i = 0; i < needClean.Count; i += 10)
+            for (int i = 0; i < restores.Count; i += 10)
             {
-                var count = Math.Min(10, needClean.Count - i);
-                new Clean(repo, needClean.GetRange(i, count)) { Log = log }.Exec();
-            }
-
-            for (int i = 0; i < needCheckout.Count; i += 10)
-            {
-                var count = Math.Min(10, needCheckout.Count - i);
-                new Restore(repo, needCheckout.GetRange(i, count), "--worktree --recurse-submodules") { Log = log }.Exec();
+                var count = Math.Min(10, restores.Count - i);
+                new Restore(repo, restores.GetRange(i, count), "--worktree --recurse-submodules") { Log = log }.Exec();
             }
         }
     }
