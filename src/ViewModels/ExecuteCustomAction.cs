b31495b89b79798dcc41b48aa3ad0002ec9977dd
leo <longshuang@msn.cn>
2025-06-27 16:02:56 +0800

    fix: wrong pattern to fill input control's value to custom action commandline arguments (#1467)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/ViewModels/ExecuteCustomAction.cs
+++ src/ViewModels/ExecuteCustomAction.cs
@@ -132,7 +132,7 @@ namespace SourceGit.ViewModels
             for (var i = ControlParameters.Count - 1; i >= 0; i--)
             {
                 var param = ControlParameters[i];
-                cmdline = cmdline.Replace($"${i}", param.GetValue());
+                cmdline = cmdline.Replace($"${i+1}", param.GetValue());
             }
 
             var log = _repo.CreateLog(CustomAction.Name);
