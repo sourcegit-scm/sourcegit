9e86a3626d792463dafdd142454c1295affffaae
Nathan Baulch <nathan.baulch@gmail.com>
2025-07-07 14:56:34 +1000

    ux: format file sizes

---
+++ src/Converters/LongConverters.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+
+namespace SourceGit.Converters
+{
+    public static class LongConverters
+    {
+        public static readonly FuncValueConverter<long, string> ToFileSize = new(bytes =>
+        {
+            var suffixes = new[] { "", "ki", "Mi", "Gi", "Ti", "Pi", "Ei" };
+            double dbl = bytes;
+            var i = 0;
+
+            while (dbl > 1024 && i < suffixes.Length - 1)
+            {
+                dbl /= 1024;
+                i++;
+            }
+
+            return $"{dbl:0.#} {suffixes[i]}B";
+        });
+    }
+}
