14adc2361a0b2da23ca2c5aae39d7725595ccda7
leo <longshuang@msn.cn>
2025-08-25 12:39:01 +0800

    enhance: warn users if they are using `vimdiff` or `nvimdiff` as external merge/diff tool (#1742)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Commands/DiffTool.cs
+++ src/Commands/DiffTool.cs
@@ -17,12 +17,15 @@ namespace SourceGit.Commands
             var tool = Native.OS.GetDiffMergeTool(true);
             if (tool == null)
             {
-                App.RaiseException(Context, "Invalid merge tool in preference setting!");
+                App.RaiseException(Context, "Invalid diff/merge tool in preference setting!");
                 return;
             }
 
             if (string.IsNullOrEmpty(tool.Cmd))
             {
+                if (!CheckGitConfiguration())
+                    return;
+
                 Args = $"difftool -g --no-prompt {_option}";
             }
             else
@@ -39,6 +42,34 @@ namespace SourceGit.Commands
             {
                 App.RaiseException(Context, ex.Message);
             }
+        }
+
+        private bool CheckGitConfiguration()
+        {
+            var config = new Config(WorkingDirectory).ReadAll();
+            if (config.TryGetValue("diff.guitool", out var guiTool))
+                return CheckCLIBasedTool(guiTool);
+            if (config.TryGetValue("merge.guitool", out var mergeGuiTool))
+                return CheckCLIBasedTool(mergeGuiTool);
+            if (config.TryGetValue("diff.tool", out var diffTool))
+                return CheckCLIBasedTool(diffTool);
+            if (config.TryGetValue("merge.tool", out var mergeTool))
+                return CheckCLIBasedTool(mergeTool);
+
+            App.RaiseException(Context, "Missing git configuration: diff.guitool");
+            return false;
+        }
+
+        private bool CheckCLIBasedTool(string tool)
+        {
+            if (tool.StartsWith("vimdiff", StringComparison.Ordinal) ||
+                tool.StartsWith("nvimdiff", StringComparison.Ordinal))
+            {
+                App.RaiseException(Context, $"CLI based diff tool \"{tool}\" is not supported by this app!");
+                return false;
+            }
+
+            return true;
         }
 
         private Models.DiffOption _option;
