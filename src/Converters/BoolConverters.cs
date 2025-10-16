ed7e6d4012bda3e85d16badcec35a327c754796c
leo <longshuang@msn.cn>
2025-10-16 15:26:35 +0800

    ux: remove `Use fixed tab width in titlebar` option from `Preferences` and makes it the only and default
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Converters/BoolConverters.cs
+++ src/Converters/BoolConverters.cs
@@ -5,9 +5,6 @@ namespace SourceGit.Converters
 {
     public static class BoolConverters
     {
-        public static readonly FuncValueConverter<bool, double> ToPageTabWidth =
-            new FuncValueConverter<bool, double>(x => x ? 200 : double.NaN);
-
         public static readonly FuncValueConverter<bool, FontWeight> IsBoldToFontWeight =
             new FuncValueConverter<bool, FontWeight>(x => x ? FontWeight.Bold : FontWeight.Regular);
 
