a6a86cd9c547e15ba3d10b1d577c9d6caa406c63
leo <longshuang@msn.cn>
2026-01-07 15:09:06 +0800

    feature: show build date in `About` dialog
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Views/About.axaml.cs
+++ src/Views/About.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Reflection;
+
 using Avalonia.Interactivity;
 
 namespace SourceGit.Views
@@ -14,6 +16,16 @@ namespace SourceGit.Views
             var ver = assembly.GetName().Version;
             if (ver != null)
                 TxtVersion.Text = $"{ver.Major}.{ver.Minor:D2}";
+
+            var meta = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
+            foreach (var attr in meta)
+            {
+                if (attr.Key.Equals("BuildDate", StringComparison.OrdinalIgnoreCase) && DateTime.TryParse(attr.Value, out var date))
+                {
+                    TxtReleaseDate.Text = App.Text("About.ReleaseDate", date.ToLocalTime().ToString("MMM d yyyy"));
+                    break;
+                }
+            }
 
             var copyright = assembly.GetCustomAttribute<AssemblyCopyrightAttribute>();
             if (copyright != null)
