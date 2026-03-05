30d5700f8815f432dd33112738074052bcc60953
leo <longshuang@msn.cn>
2026-03-05 15:48:03 +0800

    feature: add a search(filter) keyword for commands in command palette (#1937)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/ViewModels/RepositoryCommandPalette.cs
+++ src/ViewModels/RepositoryCommandPalette.cs
@@ -6,12 +6,14 @@ namespace SourceGit.ViewModels
     public class RepositoryCommandPaletteCmd
     {
         public string Label { get; set; }
+        public string Keyword { get; set; }
         public string Icon { get; set; }
         public Action Action { get; set; }
 
-        public RepositoryCommandPaletteCmd(string label, string icon, Action action)
+        public RepositoryCommandPaletteCmd(string label, string keyword, string icon, Action action)
         {
             Label = label;
+            Keyword = keyword;
             Icon = icon;
             Action = action;
         }
@@ -46,85 +48,85 @@ namespace SourceGit.ViewModels
             _launcher = launcher;
             _repo = repo;
 
-            _cmds.Add(new($"{App.Text("Blame")}...", "Blame", () =>
+            _cmds.Add(new($"{App.Text("Blame")}...", "blame", "Blame", () =>
             {
                 var sub = new BlameCommandPalette(_launcher, _repo.FullPath);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("Checkout")}...", "Check", () =>
+            _cmds.Add(new($"{App.Text("Checkout")}...", "checkout", "Check", () =>
             {
                 var sub = new CheckoutCommandPalette(_launcher, _repo);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("Compare.WithHead")}...", "Compare", () =>
+            _cmds.Add(new($"{App.Text("Compare.WithHead")}...", "compare", "Compare", () =>
             {
                 var sub = new CompareCommandPalette(_launcher, _repo, _repo.CurrentBranch);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("FileHistory")}...", "Histories", () =>
+            _cmds.Add(new($"{App.Text("FileHistory")}...", "history", "Histories", () =>
             {
                 var sub = new FileHistoryCommandPalette(_launcher, _repo.FullPath);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("Merge")}...", "Merge", () =>
+            _cmds.Add(new($"{App.Text("Merge")}...", "merge", "Merge", () =>
             {
                 var sub = new MergeCommandPalette(_launcher, _repo);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("OpenFile")}...", "OpenWith", () =>
+            _cmds.Add(new($"{App.Text("OpenFile")}...", "open", "OpenWith", () =>
             {
                 var sub = new OpenFileCommandPalette(_launcher, _repo.FullPath);
                 _launcher.OpenCommandPalette(sub);
             }));
 
-            _cmds.Add(new($"{App.Text("Repository.NewBranch")}...", "Branch.Add", () =>
+            _cmds.Add(new($"{App.Text("Repository.NewBranch")}...", "create branch", "Branch.Add", () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 repo.CreateNewBranch();
             }));
 
-            _cmds.Add(new($"{App.Text("CreateTag.Title")}...", "Tag.Add", () =>
+            _cmds.Add(new($"{App.Text("CreateTag.Title")}...", "create tag", "Tag.Add", () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 repo.CreateNewTag();
             }));
 
-            _cmds.Add(new($"{App.Text("Fetch.Title")}...", "Fetch", async () =>
+            _cmds.Add(new($"{App.Text("Fetch.Title")}...", "fetch", "Fetch", async () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 await repo.FetchAsync(false);
             }));
 
-            _cmds.Add(new($"{App.Text("Pull.Title")}...", "Pull", async () =>
+            _cmds.Add(new($"{App.Text("Pull.Title")}...", "pull", "Pull", async () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 await repo.PullAsync(false);
             }));
 
-            _cmds.Add(new($"{App.Text("Push.Title")}...", "Push", async () =>
+            _cmds.Add(new($"{App.Text("Push.Title")}...", "push", "Push", async () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 await repo.PushAsync(false);
             }));
 
-            _cmds.Add(new($"{App.Text("Stash.Title")}...", "Stashes.Add", async () =>
+            _cmds.Add(new($"{App.Text("Stash.Title")}...", "stash", "Stashes.Add", async () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
                 await repo.StashAllAsync(false);
             }));
 
-            _cmds.Add(new($"{App.Text("Apply.Title")}...", "Diff", () =>
+            _cmds.Add(new($"{App.Text("Apply.Title")}...", "apply", "Diff", () =>
             {
                 var repo = _repo;
                 _launcher.CancelCommandPalette();
@@ -171,7 +173,8 @@ namespace SourceGit.ViewModels
 
                 foreach (var cmd in _cmds)
                 {
-                    if (cmd.Label.Contains(_filter, StringComparison.OrdinalIgnoreCase))
+                    if (cmd.Label.Contains(_filter, StringComparison.OrdinalIgnoreCase) ||
+                        cmd.Keyword.Contains(_filter, StringComparison.OrdinalIgnoreCase))
                         visible.Add(cmd);
                 }
 
