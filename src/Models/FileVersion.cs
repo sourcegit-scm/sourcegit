7bf78a961044d50d1b1fda640d3f32f116c573bb
leo <longshuang@msn.cn>
2026-03-10 13:56:55 +0800

    refactor: rewrite file-history to support detecting renames (`--follow`) (#2174)
    Signed-off-by: leo <longshuang@msn.cn>

---
+++ src/Models/FileVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SourceGit.Models
+{
+    public class FileVersion
+    {
+        public string SHA { get; set; } = string.Empty;
+        public bool HasParent { get; set; } = false;
+        public User Author { get; set; } = User.Invalid;
+        public ulong AuthorTime { get; set; } = 0;
+        public string Subject { get; set; } = string.Empty;
+        public Change Change { get; set; } = new();
+
+        public string Path => Change.Path;
+        public string AuthorTimeStr => DateTime.UnixEpoch.AddSeconds(AuthorTime).ToLocalTime().ToString(DateTimeFormat.Active.DateTime);
+        public string AuthorTimeShortStr => DateTime.UnixEpoch.AddSeconds(AuthorTime).ToLocalTime().ToString(DateTimeFormat.Active.DateOnly);
+    }
+}
