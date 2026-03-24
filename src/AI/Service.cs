7758e57d7fe1a2b2dcfbce15673255596d5bcf16
leo <longshuang@msn.cn>
2026-03-24 19:19:47 +0800

    refactor: use non-streaming chat API
    Signed-off-by: leo <longshuang@msn.cn>

--- src/AI/Service.cs
+++ src/AI/Service.cs
@@ -33,6 +33,7 @@ namespace SourceGit.AI
             userMessageBuilder
                 .AppendLine("Generate a commit message (follow the rule of conventional commit message) for given git repository.")
                 .AppendLine("- Read all given changed files before generating. Do not skip any one file.")
+                .AppendLine("- Output the conventional commit message (with detail changes in list) directly. Do not explain your output nor introduce your answer.")
                 .AppendLine(string.IsNullOrEmpty(_ai.AdditionalPrompt) ? string.Empty : _ai.AdditionalPrompt)
                 .Append("Reposiory path: ").AppendLine(repo.Quoted())
                 .AppendLine("Changed files: ")
@@ -42,51 +43,38 @@ namespace SourceGit.AI
 
             do
             {
+                ChatCompletion completion = await chatClient.CompleteChatAsync(messages, options, cancellation);
                 var inProgress = false;
-                var updates = chatClient.CompleteChatStreamingAsync(messages, options).WithCancellation(cancellation);
-                var toolCalls = new ToolCallsBuilder();
-                var contentBuilder = new StringBuilder();
 
-                await foreach (var update in updates)
+                switch (completion.FinishReason)
                 {
-                    foreach (var contentPart in update.ContentUpdate)
-                        contentBuilder.Append(contentPart.Text);
+                    case ChatFinishReason.Stop:
+                        onUpdate?.Invoke(string.Empty);
+                        onUpdate?.Invoke("[Assistant]:");
+                        if (completion.Content.Count > 0)
+                            onUpdate?.Invoke(completion.Content[0].Text);
+                        else
+                            onUpdate?.Invoke("[No content was generated.]");
+                        break;
+                    case ChatFinishReason.Length:
+                        throw new Exception("The response was cut off because it reached the maximum length. Consider increasing the max tokens limit.");
+                    case ChatFinishReason.ToolCalls:
+                        {
+                            messages.Add(new AssistantChatMessage(completion));
 
-                    foreach (var toolCall in update.ToolCallUpdates)
-                        toolCalls.Append(toolCall);
-
-                    switch (update.FinishReason)
-                    {
-                        case ChatFinishReason.Stop:
-                            onUpdate?.Invoke(string.Empty);
-                            onUpdate?.Invoke("[Assistant]:");
-                            onUpdate?.Invoke(contentBuilder.ToString());
-                            break;
-                        case ChatFinishReason.Length:
-                            throw new Exception("The response was cut off because it reached the maximum length. Consider increasing the max tokens limit.");
-                        case ChatFinishReason.ToolCalls:
+                            foreach (var call in completion.ToolCalls)
                             {
-                                var calls = toolCalls.Build();
-                                var assistantMessage = new AssistantChatMessage(calls);
-                                if (contentBuilder.Length > 0)
-                                    assistantMessage.Content.Add(ChatMessageContentPart.CreateTextPart(contentBuilder.ToString()));
-                                messages.Add(assistantMessage);
-
-                                foreach (var call in calls)
-                                {
-                                    var result = await ChatTools.Process(call, onUpdate);
-                                    messages.Add(result);
-                                }
-
-                                inProgress = true;
-                                break;
+                                var result = await ChatTools.Process(call, onUpdate);
+                                messages.Add(result);
                             }
-                        case ChatFinishReason.ContentFilter:
-                            throw new Exception("Ommitted content due to a content filter flag");
-                        default:
-                            break;
-                    }
 
+                            inProgress = true;
+                            break;
+                        }
+                    case ChatFinishReason.ContentFilter:
+                        throw new Exception("Ommitted content due to a content filter flag");
+                    default:
+                        break;
                 }
 
                 if (!inProgress)
