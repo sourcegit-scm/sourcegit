ca9f5e179f25ea51946e58257dacfd21259af55a
leo <longshuang@msn.cn>
2026-03-30 11:53:57 +0800

    code_style: move some code from agent to service; rename async method with `Async` suffix; fix typos
    Signed-off-by: leo <longshuang@msn.cn>

--- src/AI/Service.cs
+++ src/AI/Service.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.ClientModel;
+using Azure.AI.OpenAI;
+using OpenAI;
+using OpenAI.Chat;
 
 namespace SourceGit.AI
 {
     public class Service
     {
-        public string Name { get; set; }
-        public string Server { get; set; }
-        public string Model { get; set; }
-        public string ApiKey { get; set; }
-        public bool ReadApiKeyFromEnv { get; set; }
-        public string AdditionalPrompt { get; set; }
-        public ApiKeyCredential Credential => new ApiKeyCredential(ReadApiKeyFromEnv ? Environment.GetEnvironmentVariable(ApiKey) : ApiKey);
+        public string Name { get; set; } = string.Empty;
+        public string Server { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public bool ReadApiKeyFromEnv { get; set; } = false;
+        public string AdditionalPrompt { get; set; } = string.Empty;
+
+        public ChatClient GetChatClient()
+        {
+            var credential = new ApiKeyCredential(ReadApiKeyFromEnv ? Environment.GetEnvironmentVariable(ApiKey) : ApiKey);
+            var client = Server.Contains("openai.azure.com/", StringComparison.Ordinal)
+                ? new AzureOpenAIClient(new Uri(Server), credential)
+                : new OpenAIClient(credential, new() { Endpoint = new Uri(Server) });
+
+            return client.GetChatClient(Model);
+        }
     }
 }
