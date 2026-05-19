sourcegit-scm/sourcegit

commit aa9290bec9b686f2a821c8ec81cd23b26406e75f

refactor: resize width of `AUTHOR` column in `HISTORY` page.
- Use a custom resizer instead of built-in `CanUserResize` property of `DataGrid` to solve the issue #1591
- Stores the width of `AUTHOR` column in repository's UI state instead of the global layout info

Signed-off-by: leo <longshuang@msn.cn>

src/Views/Histories.axaml
@@ -144,12 +144,7 @@
             </DataGridTemplateColumn.CellTemplate>
           </DataGridTemplateColumn>
 
-          <DataGridTemplateColumn MinWidth="80" CanUserResize="True" IsReadOnly="True" IsVisible="{Binding IsAuthorColumnVisible, Mode=OneWay}">
-            <DataGridTemplateColumn.Width>
-              <Binding Source="{x:Static vm:Preferences.Instance}"
-                       Path="Layout.AuthorColumnWidth"
-                       Mode="TwoWay"/>
-            </DataGridTemplateColumn.Width>
+          <DataGridTemplateColumn MinWidth="80" IsReadOnly="True" IsVisible="{Binding IsAuthorColumnVisible, Mode=OneWay}">
             <DataGridTemplateColumn.Header>
               <TextBlock Classes="table_header" Text="{DynamicResource Text.Histories.Header.Author}" HorizontalAlignment="Center"/>
             </DataGridTemplateColumn.Header>
@@ -177,7 +172,7 @@
             </DataGridTemplateColumn.CellTemplate>
           </DataGridTemplateColumn>
 
-          <DataGridTemplateColumn MinWidth="100" CanUserResize="False" IsReadOnly="True" IsVisible="{Binding IsSHAColumnVisible, Mode=OneWay}">
+          <DataGridTemplateColumn MinWidth="100" IsReadOnly="True" IsVisible="{Binding IsSHAColumnVisible, Mode=OneWay}">
             <DataGridTemplateColumn.Header>
               <TextBlock Classes="table_header" Text="{DynamicResource Text.Histories.Header.SHA}" HorizontalAlignment="Center"/>
             </DataGridTemplateColumn.Header>
@@ -193,7 +188,7 @@
             </DataGridTemplateColumn.CellTemplate>
           </DataGridTemplateColumn>
 
-          <DataGridTemplateColumn MinWidth="160" CanUserResize="False" IsReadOnly="True" IsVisible="{Binding IsDateTimeColumnVisible, Mode=OneWay}">
+          <DataGridTemplateColumn MinWidth="160" IsReadOnly="True" IsVisible="{Binding IsDateTimeColumnVisible, Mode=OneWay}">
             <DataGridTemplateColumn.Header>
               <Grid HorizontalAlignment="Center">
                 <TextBlock Classes="table_header"
@@ -222,6 +217,14 @@
           </DataGridTemplateColumn>
         </DataGrid.Columns>
       </v:HistoriesCommitList>
+      
+      <Border Background="Transparent"
+              Height="24"
+              HorizontalAlignment="Stretch" VerticalAlignment="Top"
+              Cursor="None"
+              PointerMoved="OnCommitListHeaderPointerMoved"
+              PointerPressed="OnCommitListHeaderPointerPressed"
+              PointerReleased="OnCommitListHeaderPointerReleased"/>              
 
       <v:CommitGraph x:Name="CommitGraph"
                      Margin="0,24,0,0"

src/Views/Histories.axaml.cs
@@ -104,7 +104,7 @@ namespace SourceGit.Views
         {
             SelectionMode = DataGridSelectionMode.Extended;
             CanUserReorderColumns = false;
-            CanUserResizeColumns = true;
+            CanUserResizeColumns = false;
             CanUserSortColumns = false;
             AutoGenerateColumns = false;
             IsReadOnly = true;
@@ -417,6 +417,69 @@ namespace SourceGit.Views
                 if (c != null)
                     vm.NavigateTo(c.SHA);
             }
+        }
+
+        protected override void OnDataContextChanged(EventArgs e)
+        {
+            base.OnDataContextChanged(e);
+
+            if (DataContext is ViewModels.Histories vm)
+                CommitListContainer.Columns[1].Width = new(vm.AuthorColumnWidth, DataGridLengthUnitType.Pixel);
+        }
+
+        private void OnCommitListHeaderPointerMoved(object sender, PointerEventArgs e)
+        {
+            if (sender is not Border border)
+                return;
+
+            if (DataContext is not ViewModels.Histories { IsAuthorColumnVisible: true } vm)
+                return;
+
+            var pos = e.GetPosition(border);
+            if (_resizingAuthorColumn)
+            {
+                var posX = CommitListContainer.Columns[0].ActualWidth;
+                var maxW = posX + CommitListContainer.Columns[1].ActualWidth - 100;
+                var delta = posX - pos.X;
+                var w = Math.Max(Math.Min(vm.AuthorColumnWidth + delta, maxW), 80);
+                CommitListContainer.Columns[1].Width = new(w, DataGridLengthUnitType.Pixel);
+                vm.AuthorColumnWidth = w;
+            }
+            else
+            {
+                var dis = CommitListContainer.Columns[0].ActualWidth - 4 - pos.X;
+                if (dis < 4 && dis > -4)
+                {
+                    if (border.Cursor == null)
+                        border.Cursor = new Cursor(StandardCursorType.SizeWestEast);
+                }
+                else if (border.Cursor != null)
+                {
+                    border.Cursor = null;
+                }
+            }
+        }
+
+        private void OnCommitListHeaderPointerPressed(object sender, PointerPressedEventArgs e)
+        {
+            if (sender is not Border border)
+                return;
+
+            var pos = e.GetPosition(border);
+            var dis = CommitListContainer.Columns[0].ActualWidth - 4 - pos.X;
+            if (dis > 4 || dis < -4)
+                return;
+
+            if (e.GetCurrentPoint(border).Properties.IsLeftButtonPressed)
+            {
+                _resizingAuthorColumn = true;
+                e.Handled = true;
+            }
+        }
+
+        private void OnCommitListHeaderPointerReleased(object sender, PointerReleasedEventArgs e)
+        {
+            _resizingAuthorColumn = false;
         }
 
         private void OnCommitListLayoutUpdated(object _1, EventArgs _2)
@@ -1574,5 +1637,6 @@ namespace SourceGit.Views
         private double _lastGraphStartY = 0;
         private double _lastGraphClipWidth = 0;
         private double _lastGraphRowHeight = 0;
+        private bool _resizingAuthorColumn = false;
     }
 }

src/App.JsonCodeGen.cs
@@ -51,20 +51,6 @@ namespace SourceGit
         }
     }
 
-    public class DataGridLengthConverter : JsonConverter<DataGridLength>
-    {
-        public override DataGridLength Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            var size = reader.GetDouble();
-            return new DataGridLength(size, DataGridLengthUnitType.Pixel, 0, size);
-        }
-
-        public override void Write(Utf8JsonWriter writer, DataGridLength value, JsonSerializerOptions options)
-        {
-            writer.WriteNumberValue(value.DisplayValue);
-        }
-    }
-
     [JsonSourceGenerationOptions(
         WriteIndented = true,
         IgnoreReadOnlyFields = true,
@@ -73,7 +59,6 @@ namespace SourceGit
             typeof(DateTimeConverter),
             typeof(ColorConverter),
             typeof(GridLengthConverter),
-            typeof(DataGridLengthConverter),
         ]
     )]
     [JsonSerializable(typeof(Models.ExternalToolCustomization))]

src/Models/RepositoryUIStates.cs
@@ -33,6 +33,12 @@ namespace SourceGit.Models
             set;
         } = true;
 
+        public double AuthorColumnWidth
+        {
+            get;
+            set;
+        } = 120;
+
         public bool EnableTopoOrderInHistory
         {
             get;

src/ViewModels/Histories.cs
@@ -152,6 +152,12 @@ namespace SourceGit.ViewModels
             }
         }
 
+        public double AuthorColumnWidth
+        {
+            get => _repo.UIStates.AuthorColumnWidth;
+            set => _repo.UIStates.AuthorColumnWidth = value;
+        }
+
         public bool IsOpenAsStandaloneVisible
         {
             get => DetailContext is CommitDetail or RevisionCompare;

src/ViewModels/LayoutInfo.cs
@@ -65,17 +65,10 @@ namespace SourceGit.ViewModels
             set => SetProperty(ref _commitDetailFilesLeftWidth, value);
         }
 
-        public DataGridLength AuthorColumnWidth
-        {
-            get => _authorColumnWidth;
-            set => SetProperty(ref _authorColumnWidth, new DataGridLength(value.Value, DataGridLengthUnitType.Pixel, 0, value.DisplayValue));
-        }
-
         private GridLength _repositorySidebarWidth = new GridLength(250, GridUnitType.Pixel);
         private GridLength _workingCopyLeftWidth = new GridLength(300, GridUnitType.Pixel);
         private GridLength _stashesLeftWidth = new GridLength(300, GridUnitType.Pixel);
         private GridLength _commitDetailChangesLeftWidth = new GridLength(256, GridUnitType.Pixel);
         private GridLength _commitDetailFilesLeftWidth = new GridLength(256, GridUnitType.Pixel);
-        private DataGridLength _authorColumnWidth = new DataGridLength(120, DataGridLengthUnitType.Pixel, 0, 120);
     }
 }

commit b7e4d32b1e9edf7330080c14fa3251bf1fc7ffd9

fix: context menu missing caused by commit aa9290bec9
Signed-off-by: leo <longshuang@msn.cn>

src/Views/Histories.axaml
@@ -221,10 +221,10 @@
       <Border Background="Transparent"
               Height="24"
               HorizontalAlignment="Stretch" VerticalAlignment="Top"
-              Cursor="None"
               PointerMoved="OnCommitListHeaderPointerMoved"
               PointerPressed="OnCommitListHeaderPointerPressed"
-              PointerReleased="OnCommitListHeaderPointerReleased"/>              
+              PointerReleased="OnCommitListHeaderPointerReleased"
+              ContextRequested="OnCommitListHeaderContextRequested"/>              
 
       <v:CommitGraph x:Name="CommitGraph"
                      Margin="0,24,0,0"

src/Views/Histories.axaml.cs
@@ -450,12 +450,12 @@ namespace SourceGit.Views
                 var dis = CommitListContainer.Columns[0].ActualWidth - 4 - pos.X;
                 if (dis < 4 && dis > -4)
                 {
-                    if (border.Cursor == null)
-                        border.Cursor = new Cursor(StandardCursorType.SizeWestEast);
+                    if (border.Cursor != _resizingCursor)
+                        border.Cursor = _resizingCursor;
                 }
-                else if (border.Cursor != null)
+                else if (border.Cursor != Cursor.Default)
                 {
-                    border.Cursor = null;
+                    border.Cursor = Cursor.Default;
                 }
             }
         }
@@ -480,6 +480,57 @@ namespace SourceGit.Views
         private void OnCommitListHeaderPointerReleased(object sender, PointerReleasedEventArgs e)
         {
             _resizingAuthorColumn = false;
+        }
+
+        private void OnCommitListHeaderContextRequested(object sender, ContextRequestedEventArgs e)
+        {
+            if (DataContext is not ViewModels.Histories vm)
+                return;
+
+            if (sender is not Border border)
+                return;
+
+            var columnsHeader = new MenuItem();
+            columnsHeader.Header = new TextBlock() { Text = App.Text("Histories.ShowColumns"), FontWeight = FontWeight.Bold };
+            columnsHeader.IsEnabled = false;
+
+            var authorColumn = new MenuItem();
+            authorColumn.Header = App.Text("Histories.Header.Author");
+            if (vm.IsAuthorColumnVisible)
+                authorColumn.Icon = this.CreateMenuIcon("Icons.Check");
+            authorColumn.Click += (_, ev) =>
+            {
+                vm.IsAuthorColumnVisible = !vm.IsAuthorColumnVisible;
+                ev.Handled = true;
+            };
+
+            var shaColumn = new MenuItem();
+            shaColumn.Header = App.Text("Histories.Header.SHA");
+            if (vm.IsSHAColumnVisible)
+                shaColumn.Icon = this.CreateMenuIcon("Icons.Check");
+            shaColumn.Click += (_, ev) =>
+            {
+                vm.IsSHAColumnVisible = !vm.IsSHAColumnVisible;
+                ev.Handled = true;
+            };
+
+            var timeColumn = new MenuItem();
+            timeColumn.Header = App.Text("Histories.Header.DateTime");
+            if (vm.IsDateTimeColumnVisible)
+                timeColumn.Icon = this.CreateMenuIcon("Icons.Check");
+            timeColumn.Click += (_, ev) =>
+            {
+                vm.IsDateTimeColumnVisible = !vm.IsDateTimeColumnVisible;
+                ev.Handled = true;
+            };
+
+            var menu = new ContextMenu();
+            menu.Items.Add(columnsHeader);
+            menu.Items.Add(authorColumn);
+            menu.Items.Add(shaColumn);
+            menu.Items.Add(timeColumn);
+            menu.Open(border);
+            e.Handled = true;
         }
 
         private void OnCommitListLayoutUpdated(object _1, EventArgs _2)
@@ -532,82 +583,29 @@ namespace SourceGit.Views
 
         private void OnCommitListContextRequested(object sender, ContextRequestedEventArgs e)
         {
-            if (e.Source is Control { DataContext: Models.Commit })
+            var repoView = this.FindAncestorOfType<Repository>();
+            if (repoView is not { DataContext: ViewModels.Repository repo })
+                return;
+
+            var selected = CommitListContainer.SelectedItems;
+            if (selected is not { Count: > 0 })
+                return;
+
+            var commits = new List<Models.Commit>();
+            for (var i = selected.Count - 1; i >= 0; i--)
             {
-                var repoView = this.FindAncestorOfType<Repository>();
-                if (repoView is not { DataContext: ViewModels.Repository repo })
-                    return;
-
-                var selected = CommitListContainer.SelectedItems;
-                if (selected is not { Count: > 0 })
-                    return;
-
-                var commits = new List<Models.Commit>();
-                for (var i = selected.Count - 1; i >= 0; i--)
-                {
-                    if (selected[i] is Models.Commit c)
-                        commits.Add(c);
-                }
-
-                if (selected.Count > 1)
-                {
-                    var menu = CreateContextMenuForMultipleCommits(repo, commits);
-                    menu.Open(CommitListContainer);
-                }
-                else if (selected.Count == 1)
-                {
-                    var menu = CreateContextMenuForSingleCommit(repo, commits[0]);
-                    menu.Open(CommitListContainer);
-                }
+                if (selected[i] is Models.Commit c)
+                    commits.Add(c);
             }
-            else if (e.Source is Control elem)
+
+            if (selected.Count > 1)
             {
-                var headersPresenter = CommitListContainer.FindDescendantOfType<DataGridColumnHeadersPresenter>();
-                if (!headersPresenter.IsVisualAncestorOf(elem))
-                    return;
-
-                if (DataContext is not ViewModels.Histories vm)
-                    return;
-
-                var columnsHeader = new MenuItem();
-                columnsHeader.Header = new TextBlock() { Text = App.Text("Histories.ShowColumns"), FontWeight = FontWeight.Bold };
-                columnsHeader.IsEnabled = false;
-
-                var authorColumn = new MenuItem();
-                authorColumn.Header = App.Text("Histories.Header.Author");
-                if (vm.IsAuthorColumnVisible)
-                    authorColumn.Icon = this.CreateMenuIcon("Icons.Check");
-                authorColumn.Click += (_, ev) =>
-                {
-                    vm.IsAuthorColumnVisible = !vm.IsAuthorColumnVisible;
-                    ev.Handled = true;
-                };
-
-                var shaColumn = new MenuItem();
-                shaColumn.Header = App.Text("Histories.Header.SHA");
-                if (vm.IsSHAColumnVisible)
-                    shaColumn.Icon = this.CreateMenuIcon("Icons.Check");
-                shaColumn.Click += (_, ev) =>
-                {
-                    vm.IsSHAColumnVisible = !vm.IsSHAColumnVisible;
-                    ev.Handled = true;
-                };
-
-                var timeColumn = new MenuItem();
-                timeColumn.Header = App.Text("Histories.Header.DateTime");
-                if (vm.IsDateTimeColumnVisible)
-                    timeColumn.Icon = this.CreateMenuIcon("Icons.Check");
-                timeColumn.Click += (_, ev) =>
-                {
-                    vm.IsDateTimeColumnVisible = !vm.IsDateTimeColumnVisible;
-                    ev.Handled = true;
-                };
-
-                var menu = new ContextMenu();
-                menu.Items.Add(columnsHeader);
-                menu.Items.Add(authorColumn);
-                menu.Items.Add(shaColumn);
-                menu.Items.Add(timeColumn);
+                var menu = CreateContextMenuForMultipleCommits(repo, commits);
+                menu.Open(CommitListContainer);
+            }
+            else if (selected.Count == 1)
+            {
+                var menu = CreateContextMenuForSingleCommit(repo, commits[0]);
                 menu.Open(CommitListContainer);
             }
 
@@ -1638,5 +1636,6 @@ namespace SourceGit.Views
         private double _lastGraphClipWidth = 0;
         private double _lastGraphRowHeight = 0;
         private bool _resizingAuthorColumn = false;
+        private Cursor _resizingCursor = new Cursor(StandardCursorType.SizeWestEast);
     }
 }