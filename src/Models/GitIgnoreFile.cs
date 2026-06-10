6909f2205087c1710e86b984260d8542ef8f32ce
leo <longshuang@msn.cn>
2026-06-10 12:20:35 +0800

    feature: supports to save ignore file pattern to sub-directory (#2424)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Models/GitIgnoreFile.cs
+++ src/Models/GitIgnoreFile.cs
@@ -1,26 +1,36 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using Avalonia.Media;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SourceGit.Models
 {
-    public class GitIgnoreFile
+    public record GitIgnoreFile(string DisplayName, string FullPath, string Pattern, bool IsLocalOnly)
     {
-        public static readonly List<GitIgnoreFile> Supported = [new(true), new(false)];
-
-        public bool IsShared { get; set; }
-        public string File => IsShared ? ".gitignore" : "<git_dir>/info/exclude";
-        public string Desc => IsShared ? "Shared" : "Private";
-        public IBrush Brush => IsShared ? Brushes.Green : Brushes.Gray;
-
-        public GitIgnoreFile(bool isShared)
+        public static List<GitIgnoreFile> GetSupported(string repo, string gitDir, string pattern)
         {
-            IsShared = isShared;
-        }
+            var supported = new List<GitIgnoreFile>();
 
-        public string GetFullPath(string repoPath, string gitDir)
-        {
-            return IsShared ? Path.Combine(repoPath, ".gitignore") : Path.Combine(gitDir, "info", "exclude");
+            // .gitignore in repository root.
+            supported.Add(new(".gitignore", $"{repo}/.gitignore", pattern, false));
+
+            // If pattern points to a file/directory that in sub-directory of repository, we should also support .gitignore in that sub-directory.
+            var normalizedPattern = pattern.Replace('\\', '/').TrimEnd('/');
+            var lastDirIdx = normalizedPattern.LastIndexOf('/');
+            if (lastDirIdx > 0)
+            {
+                var parentDir = normalizedPattern.Substring(0, lastDirIdx);
+                var overridedPattern = normalizedPattern.Substring(lastDirIdx + 1);
+                supported.Add(new($"{parentDir}/.gitignore", $"{repo}/{parentDir}/.gitignore", overridedPattern, false));
+            }
+
+            // .git/info/exclude in git directory.
+            var normalizedGitDir = gitDir.Replace('\\', '/');
+            var testGitDir = $"{repo}/.git".Replace('\\', '/');
+            if (normalizedGitDir.Equals(testGitDir, StringComparison.Ordinal))
+                supported.Add(new(".git/info/exclude", $"{normalizedGitDir}/info/exclude", pattern, true));
+            else
+                supported.Add(new(".git/info/exclude", $"{gitDir}/info/exclude", pattern, true));
+
+            return supported;
         }
     }
 }
