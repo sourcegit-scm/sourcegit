6f670b19b8a6df8e587f678da2cd1a2f6f3774d4
leo <longshuang@msn.cn>
2026-07-23 11:54:34 +0800

    feature: support `open -a SourceGit <path>` on macOS (#2553)
    Signed-off-by: leo <longshuang@msn.cn>

--- src/App.axaml.cs
+++ src/App.axaml.cs
@@ -11,6 +11,7 @@ using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Media.Fonts;
+using Avalonia.Platform;
 using Avalonia.Styling;
 using Avalonia.Threading;
 
@@ -474,6 +475,15 @@ namespace SourceGit
 
             Native.OS.SetupExternalTools();
             Models.AvatarManager.Instance.Start();
+
+            if (this.TryGetFeature<IActivatableLifetime>() is { } activatable)
+            {
+                activatable.Activated += (_, e) =>
+                {
+                    if (e is FileActivatedEventArgs { Files: { Count: > 0 } } fileArgs)
+                        _launcher?.TryOpenRepositoryFromPath(fileArgs.Files[0].Path.LocalPath);
+                };
+            }
 
             string startupRepo = null;
             if (desktop.Args is { Length: 1 })
