ab043adb70e0351152ecd44d91bb8c79de20e6d7
Christiaan <github@tnrd.net>
2026-07-28 20:14:43 +0200

    chore: rename SGit to SourceGit for clarity

--- src/Models/IpcChannel.cs
+++ src/Models/IpcChannel.cs
@@ -74,7 +74,7 @@ namespace SourceGit.Models
             var dataDir = Native.OS.DataDir.Replace('\\', '/').TrimEnd('/');
             var hash = SHA256.HashData(Encoding.UTF8.GetBytes(dataDir));
             var hashStr = Convert.ToHexString(hash)[..8];
-            return $"SGit{Environment.UserName}_{hashStr}";
+            return $"SourceGit{Environment.UserName}_{hashStr}";
         }
 
         private async void StartServer()
