ce072094dc0c90779d40a8becbf8f80e906afb90
leo <longshuang@msn.cn>
2026-08-28 16:26:42 +0800

    feature: support to terminate `Fetch <upstream> into <local>`
    Signed-off-by: leo <longshuang@msn.cn>

--- src/ViewModels/FetchInto.cs
+++ src/ViewModels/FetchInto.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace SourceGit.ViewModels
 {
@@ -19,6 +20,7 @@ namespace SourceGit.ViewModels
             _repo = repo;
             Local = local;
             Upstream = upstream;
+            CanTerminate = true;
         }
 
         public override async Task<bool> Sure()
@@ -29,21 +31,33 @@ namespace SourceGit.ViewModels
             var log = _repo.CreateLog($"Fetch Into '{Local.FriendlyName}'");
             Use(log);
 
+            _cancellation = new CancellationTokenSource();
+            var token = _cancellation.Token;
+
             await new Commands.Fetch(_repo.FullPath, Local, Upstream)
+                .WithCancellation(token)
                 .Use(log)
                 .RunAsync();
 
             log.Complete();
 
-            if (_repo.SelectedViewIndex == 0)
+            if (_repo.SelectedViewIndex == 0 && !token.IsCancellationRequested)
             {
                 var newHead = await new Commands.QueryRevisionByRefName(_repo.FullPath, Local.Name).GetResultAsync();
                 _repo.NavigateToCommit(newHead, true);
             }
 
+            _cancellation = null;
             return true;
         }
 
+        public override void Terminate()
+        {
+            // Just fire cancel event and UI will auto wait the `Sure` complete
+            var _ = _cancellation?.CancelAsync();
+        }
+
         private readonly Repository _repo = null;
+        private CancellationTokenSource _cancellation = null;
     }
 }
