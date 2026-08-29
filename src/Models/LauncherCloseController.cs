89a14929d77f510e01a9953a4c2a33d23ac73853
Hieu Dam <86464713+dhhieu113pro@users.noreply.github.com>
2026-08-29 07:34:31 +0700

    feat: add launcher close decision controller

---
+++ src/Models/LauncherCloseController.cs
@@ -0,0 +1,41 @@
+namespace SourceGit.Models
+{
+    public enum CloseAppDecision
+    {
+        Yes,
+        No,
+        AddToTray,
+    }
+
+    public enum LauncherCloseAction
+    {
+        Confirm,
+        Exit,
+        KeepOpen,
+        HideToTray,
+    }
+
+    public sealed class LauncherCloseController
+    {
+        public LauncherCloseAction OnCloseRequested()
+        {
+            return _exitRequested ? LauncherCloseAction.Exit : LauncherCloseAction.Confirm;
+        }
+
+        public LauncherCloseAction Apply(CloseAppDecision decision)
+        {
+            switch (decision)
+            {
+                case CloseAppDecision.Yes:
+                    _exitRequested = true;
+                    return LauncherCloseAction.Exit;
+                case CloseAppDecision.AddToTray:
+                    return LauncherCloseAction.HideToTray;
+                default:
+                    return LauncherCloseAction.KeepOpen;
+            }
+        }
+
+        private bool _exitRequested;
+    }
+}
