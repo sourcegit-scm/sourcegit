4fcfc8e54e6566567e83d1ad3cabb7986ef5a416
leo <longshuang@msn.cn>
2026-09-08 16:25:30 +0800

    enhance: stop searching commits after collapsed sidebar
    Signed-off-by: leo <longshuang@msn.cn>

--- src/Views/RepositoryToolbar.axaml.cs
+++ src/Views/RepositoryToolbar.axaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Media;
@@ -16,6 +17,19 @@ namespace SourceGit.Views
         public RepositoryToolbar()
         {
             InitializeComponent();
+        }
+
+        private void OnToggleSidebar(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton { IsChecked: true } button)
+            {
+                var launcher = App.GetLauncher();
+                foreach (var page in launcher.Pages)
+                {
+                    if (page.Data is ViewModels.Repository repo)
+                        repo.IsSearchingCommits = false;
+                }
+            }
         }
 
         private void OpenWithExternalTools(object sender, RoutedEventArgs ev)
